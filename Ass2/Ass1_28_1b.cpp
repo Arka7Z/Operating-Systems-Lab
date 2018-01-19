ea2465a1ac6e368fd7750377a8dd9ba09e77ac00
Arka Pal <arkapal.pal@gmail.com>
2018-01-19 23:38:08 +0530

    CHECK THE LAST MONDAY! Hue hue
    support pipe commands in shell

--- Ass2/Ass1_28_1b.cpp
+++ Ass2/Ass1_28_1b.cpp
@@ -11,7 +11,95 @@
 
 using namespace std;
 
+void parser(const char *line , char **cmd_args);
+vector<string> split(const string &s, char delim);
 
+vector<string> split(const string &s, char delim)
+{
+    stringstream ss(s);
+    string item;
+    vector<string> tokens;
+    while (getline(ss, item, delim))
+    {
+        tokens.push_back(item);
+    }
+    return tokens;
+}
+
+
+int spawn_proc (int in, int out, char **cmd)
+{
+  pid_t pid;
+
+  if ((pid = fork ()) == 0)
+    {
+      if (in != 0)
+        {
+          dup2 (in, 0);
+          close (in);
+        }
+
+      if (out != 1)
+        {
+          dup2 (out, 1);
+          close (out);
+        }
+
+      return execvp (*cmd, cmd);
+    }
+
+  return pid;
+}
+
+
+int pipe_Command()
+{
+  printf("shellby-$ ");
+  string line,file;
+  getchar();
+  char *cmd_args[64];
+  getline(cin,line);
+  vector<string> commands=split(line,'|');
+  //list<char* cmd_args[64] > command_list(commands.size());
+  //  for(int i=0;i<commands.size();i++)
+  //   cout<<commands[i]<<endl;
+
+  //////
+  int n=commands.size();
+  int i;
+  pid_t pid;
+  int in, fd [2];
+
+  /* The first process should get its input from the original file descriptor 0.  */
+  in = 0;
+
+  /* Note the loop bound, we spawn here all, but the last stage of the pipeline.  */
+  for (i = 0; i < n - 1; ++i)
+    {
+      pipe (fd);
+      char* cmd_args[64];
+      parser(commands[i].c_str(),cmd_args);
+      /* f [1] is the write end of the pipe, we carry `in` from the prev iteration.  */
+      spawn_proc (in, fd [1], cmd_args);
+
+      /* No need for the write end of the pipe, the child will write here.  */
+      close (fd [1]);
+
+      /* Keep the read end of the pipe, the next child will read from there.  */
+      in = fd [0];
+    }
+
+  /* Last stage of the pipeline - set stdin be the read end of the previous pipe
+     and output to the original file descriptor 1. */
+  if (in != 0)
+    dup2 (in, 0);
+
+  /* Execute the last stage with the current process. */
+
+  parser(commands[i].c_str(),cmd_args);
+  return execvp (*cmd_args, cmd_args);
+
+}
 void create_proc(const char *line , char **cmd_args,bool directed=false,bool in=true,string filename="",bool background=false)
 {
   int status;
@@ -141,7 +229,7 @@ int main()
           else if(option== "E")
                 select_Proc(false,true,true);
           else   if(option== "F")
-                ;
+                pipe_Command();
           else   if(option== "G")
                 exit(1);
       }
