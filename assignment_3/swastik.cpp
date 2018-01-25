2c5374c5861220807e666c54bbbbee169891aa8c
Arka Pal <arkapal.pal@gmail.com>
2018-01-25 15:04:11 +0530

    added interface to input n_producer & n_cosumer

--- assignment_3/swastik.cpp
+++ assignment_3/swastik.cpp
@@ -6,6 +6,7 @@
 #include <time.h>
 
 #define buf_size 5
+#define prime_lim 10000
 
 typedef struct
 {
@@ -17,18 +18,37 @@ typedef struct
 
 using namespace std;
 
-
+vector<int> primes;
 int time_to_wait = 30;
-int num_consumer = 10, num_producer = 8;
 
-void consumer(int shm_id , key_t mem_key,int consumer_no)
+
+void SieveOfEratosthenes()
+{
+
+    int n=prime_lim;
+    bool prime[n+1];
+    memset(prime, true, sizeof(prime));
+
+    for (int p=2; p*p<=n; p++)
+    {
+        // If prime[p] is not changed, then it is a prime
+        if (prime[p] == true)
+        {
+            // Update all multiples of p
+            for (int i=p*2; i<=n; i += p)
+                prime[i] = false;
+        }
+    }
+
+    for (int p=2; p<=n; p++)
+       if (prime[p])
+          primes.push_back(p);
+}
+
+void consumer(int shm_id ,int consumer_no)
 {
   SHM* shm_ptr = (SHM *)shmat(shm_id , NULL, 0);
-
   int tmp,num;
-
-
-
   while(true)
     {
       sleep(rand()%6);
@@ -39,23 +59,19 @@ void consumer(int shm_id , key_t mem_key,int consumer_no)
     	  num=shm_ptr->ar[tmp-1];
         time_t t;
         time(&t);
-    	 cout <<"consumer "<<consumer_no<<" number captured = "<<num<< " in process : "<< getpid() << ": time - "<< t <<endl;
+    	 cout <<"Consumer "<<consumer_no<<" : "<<num<<", time: "<< t <<endl;
     	}
     }
 
-
-
-
-
-  shmdt(shm_ptr);
+    shmdt(shm_ptr);
 
 }
 
-void producer(int shm_id , key_t mem_key, int producer_no)
+void producer(int shm_id , int producer_no)
 {
   SHM* shm_ptr = (SHM *)shmat(shm_id , NULL, 0);
 
-  int gen = rand()%100;
+  int gen;
   int tmp;
 
   while(true)
@@ -65,13 +81,12 @@ void producer(int shm_id , key_t mem_key, int producer_no)
             if(tmp<5)
       	{
       	  shm_ptr->cur_size++;
-      	  shm_ptr->ar[tmp]=gen;
+          gen = rand()%(primes.size());
+      	  shm_ptr->ar[tmp]=primes[gen];
           time_t t;
           time(&t);
-
-          cout <<"Producer "<<producer_no<<" number produced = "<<gen<< " in process : "<< getpid() << ": time - "<< t <<endl;
-
-      	}
+          cout <<"Producer "<<producer_no<<": "<<shm_ptr->ar[tmp]<< ",time : "<< t <<endl;
+        }
     }
 
 
@@ -81,12 +96,18 @@ void producer(int shm_id , key_t mem_key, int producer_no)
 int main()
 {
   vector<int > pids;
+  SieveOfEratosthenes();
   int shm_id,id;
   key_t mem_key;
   int *shm_ptr;
   mem_key = ftok(".",'a');
   shm_id = shmget(mem_key, shm_size , IPC_CREAT | 0777);
 
+  int num_producer,num_consumer;
+  cout<<"Enter the number of producers"<<endl;
+  cin>>num_producer;
+  cout<<"Enter the number of Consumers"<<endl;
+  cin>>num_consumer;
   if(shm_id < 0)
     {
       perror("shmget failed");
@@ -98,8 +119,8 @@ int main()
       id = fork();
       if(id==0)
       	{
-      	  srand(i*i);
-      	  producer(shm_id, mem_key,i+1);
+      	  srand(i);
+      	  producer(shm_id,i+1);
       	}
             else
       	{
@@ -112,8 +133,8 @@ int main()
       id=fork();
             if(id==0)
       	{
-      	  srand(i*i);
-      	  consumer(shm_id,mem_key,i+1);
+      	  srand(i+num_consumer);
+      	  consumer(shm_id,i+1);
       	}
             else
       	{
@@ -122,13 +143,10 @@ int main()
     }
 
   sleep(30);
-
   for(int i=0;i<pids.size();i++)
     {
       kill(pids[i],SIGKILL);
     }
-
   shmctl(shm_id,IPC_RMID,NULL);
-
   return 1;
 }
