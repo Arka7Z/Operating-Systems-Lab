f798879f52b08c16986a1f6426e3fcb8da423cec
basher666 <swastik.haldar@gmail.com>
2018-02-10 17:51:15 +0530

    thread scheduler working

--- ass3b/thread_scheduler.cpp
+++ ass3b/thread_scheduler.cpp
@@ -6,6 +6,8 @@
 #include <stdio.h>
 #include <time.h>
 
+#define bufsize 30000
+
 using namespace std;
 
 int * status;
@@ -15,43 +17,30 @@ typedef struct
   int n;
 } sched_args;
 
+void sigusr1_handler(int);
+void sigusr2_handler(int);
+
 void sigusr2_handler(int signum)
 {
-  //signal(SIGUSR1,sigusr1_handler);
-  signal(SIGUSR2,sigusr2_handler);
-  cout<<"back to work - "<<pthread_self()<<endl;
+  
+  //cout<<"back to work - "<<pthread_self()<<endl;
+  // signal(SIGUSR1,sigusr1_handler);
+  // signal(SIGUSR2,sigusr2_handler);
+  cout<<"returning from sigusr2_handler()"<<endl;
 }
+
 void sigusr1_handler(int signum)
 {
-  signal(SIGUSR1,sigusr1_handler);
-  signal(SIGUSR2,sigusr2_handler);
-  pause();
-}
-
-
-
-void* worker_func(void * tmp)
-{
-  int w_id = *((int *)tmp);
-  usleep(100000);
-  signal(SIGUSR1,sigusr1_handler);
-  signal(SIGUSR2,sigusr2_handler);
-
-  int ar[1000];
-
-  srand(time(NULL));
-
-  for(int i=0;i<1000;i++)
+  if(signum==SIGUSR1)
     {
-      ar[i]=rand()%10000;
+      //cout<<"sigusr1 handled"<<endl;
+      // signal(SIGUSR1,sigusr1_handler);
+      // signal(SIGUSR2,sigusr2_handler);
+      //cout<<"thread - "<<pthread_self()<<" pausing"<<endl;
+      pause();
+      cout<<"returning from sigusr1_handler()"<<endl;
     }
-  sort(ar,ar+1000);
-
-  sleep(rand()%11+1);
-  status[w_id]=2;
-  while(1);
 }
-
 
 void sigint_handler(int signum)
 {
@@ -59,11 +48,65 @@ void sigint_handler(int signum)
   if(signum==SIGINT)
     cout<<"HEHE"<<endl;
   signal(SIGINT,sigint_handler);
+}
+
+void* worker_func(void * tmp)
+{
+  //signal(SIGUSR1,sigusr1_handler);
+  //signal(SIGUSR2,sigusr2_handler);
   
+  int * w_id = (int *)tmp;
+  //usleep(100);
+
+  int ar[bufsize];
+
+  srand(time(NULL));
+
+  for(int i=0;i<bufsize;i++)
+    {
+      //ar[i]= i > 0 ? (ar[i-1] * 23) % 10007 : *w_id;
+      ar[i]=(rand()%10007+1);
+    }
+  //printf("generated in worker in %d\n",*w_id);
+  fflush(stdout);
+
+  int min_no=INT_MIN;
+  int min_pos=-1;
+  int swap;
+  for(int i=0;i<bufsize-1;i++)
+    {
+      min_no=ar[i];
+      min_pos=i;
+      for(int j=i+1;j<bufsize;j++)
+	{
+	  if(min_no>ar[j])
+	    {
+	      min_no=ar[j];
+	      min_pos=j;
+	    }
+	}
+      if(min_no<ar[i])
+	{
+	  swap=ar[min_pos];
+	  ar[min_pos]=ar[i];
+	  ar[i]=swap;
+	}
+      
+    }
+  
+  //printf("sorted in worker in %d\n",*w_id);
+  fflush(stdout);
+  sleep(rand()%11+1);
+  //printf("worker_func() %d completed\n",*w_id);
+  status[*w_id]=2;
+
+  pthread_exit(0);
+  //while(1);
 }
 
 void* scheduler_func(void * args)
 {
+  cout << "scheduler thread started"<<endl;
   sched_args * util = (sched_args *) args;
   pthread_t * workers = util->w;
   int n = util->n;
@@ -73,76 +116,102 @@ void* scheduler_func(void * args)
   
   for(int i=0;i<n;i++)
     {
-      pthread_kill(workers[i],SIGUSR1);
-    }
-  for(int i=0;i<n;i++)
-    {
       ready.push(i);
     }
-  while(1)
+  
+  while(not ready.empty())
     {
       int w_id = ready.front();
       pthread_kill(workers[w_id],SIGUSR2);
+      //printf("running worker %d\n",w_id);
       status[w_id]=1;
       sleep(1);
+      
       if(status[w_id]==2)
 	{
-	  pthread_kill(workers[w_id],SIGKILL);
+	  
+	  //pthread_kill(workers[w_id],SIGKILL);
+	  //printf("finished thread %d \n",w_id);
+	  pthread_cancel(workers[w_id]);
  	  ready.pop();
 	  running_n--;
-	  if(running_n==0)
+	  //cout<<"finished removing thread from queue"<<endl;
+	  //printf("ready size = %d\n",ready.size());
+	  //printf("running = %d\n",running_n);
+	  if(running_n==0 || ready.empty())
 	    break;
 	}
-      else
+      else if(status[w_id]==1)
 	{
+
 	  pthread_kill(workers[w_id],SIGUSR1);
+
 	  status[w_id]=0;
+	  //printf("worker %d is pausing\n",w_id);
+	  //printf("ready size = %d\n",ready.size());
+	  
 	  int t = ready.front();
 	  ready.pop();
 	  ready.push(t);
+	  
 	}
+      else
+	cout<<"strange error \n";
       
     }
-  pthread_exit(0);
 }
+
 void* reporter_func(void *nn)
 {
+  cout<<"reporter thread started"<<endl;
   int n = *((int *) nn);
   int prev_running=-1;
-
+  int ack[n];
+  memset(status,0,n*sizeof(int));
+  
   int terminated=0;
   while(terminated!=n)
     {
+      //cout<<"polling"<<endl;
       terminated=0;
       for(int i=0;i<n;i++)
 	{
 	  if(status[i]==1)
 	    {
-	      if(prev_running>=0 && prev_running!=i) 
+	      //printf("found a running thread\n");
+	      if(prev_running<0 || prev_running!=i) 
 		{
-		  cout<<"worker - "<<i<<" has paused "<<endl;
-		  cout<<"worker - "<<i<<" is running"<<endl;
+		  printf("worker - %d has paused\n",prev_running);
+		  printf("worker - %d is running\n",i);
 		  prev_running=i;
 		}
 	    }
 	  else if(status[i]==2)
 	    {
+	      if(ack[i]==0)
+		{
+		  ack[i]=1;
+		  printf("worker - %d has stopped\n",i);
+		}
 	      terminated++;
 	    }
 	}
     }
-  int i=0;
-  pthread_exit(0);
-	  
+  //pthread_exit(0)
 }
+
 int main()
 {
-  signal(SIGINT,sigint_handler);
+  //signal(SIGINT,sigint_handler);
+
+  signal(SIGUSR1,sigusr1_handler);
+  signal(SIGUSR2,sigusr2_handler);
 
   int n;
   cout << "enter N " << endl;
   cin >> n;
 
+  
   pthread_attr_t *attr=NULL;
   status = (int *)malloc(sizeof(int)*n);
   memset(status,0,n*sizeof(int));
@@ -150,20 +219,26 @@ int main()
   pthread_t workers[n];
   pthread_t sched_t;
   pthread_t report_t;
-
+  int id[n];
   for(int i=0;i<n;i++)
     {
-      pthread_create(&workers[i],attr,worker_func,(void *)&i);
+      id[i]=i;
+      pthread_create(&workers[i],attr,worker_func,(void *)&id[i]);
+      pthread_kill(workers[i],SIGUSR1);
     }
-  
+
   sched_args util;
   util.w = &workers[0];
   util.n = n;
   pthread_create(&sched_t,attr,scheduler_func,(void *)&util);
 
+  cout<<"check1 in main()"<<endl;
   pthread_create(&report_t,attr,reporter_func,(void *)&n);
   void ** ret;
-  pthread_join(sched_t,ret);
-  pthread_join(report_t,ret);
+  pthread_join(sched_t, NULL);
+  
+  cout<<"scheduler finished"<<endl;
+  pthread_join(report_t,NULL);
+  cout <<"reporter finished"<<endl;
   return EXIT_SUCCESS;
 }
